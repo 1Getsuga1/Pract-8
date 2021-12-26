263b318a41f2ed893a2cc66ecd7eed2bea6d8cde
Кирилл Соколов <sokolov17925@yandex.ru>
2021-12-26 21:06:11 +0300

    Добавьте файлы проекта.

---
+++ Pract-8/IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_8
+{
+    interface IPerson
+    {
+        string FirstName { get; set; }
+        string ShowNamePatronymic();
+    }
+}
